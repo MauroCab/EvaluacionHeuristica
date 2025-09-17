19d6156a29980d7a8354a3083a5d8bd2752e45f4
Mauro Cabrera <maurocabricci17@gmail.com>
2025-09-17 07:24:34 -0300

    Agregar archivos de proyecto.

---
+++ EvaluacionEuristica_Form/FormEH.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EvaluacionEuristica_Form
+{
+    public partial class FormEH : Form
+    {
+        public FormEH()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEvaluarDiseño_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEvaluarNav_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEvaluarCont_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEvaluarBusq_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ EvaluacionEuristica_Form/FormNav.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace EvaluacionEuristica_Form
+{
+    partial class FormNav
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormNav
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 450);
+            this.Name = "FormNav";
+            this.Text = "FormNav";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
