129196796cd15e2680bfc805f0677c107170a6a1
Mauro Cabrera <maurocabricci17@gmail.com>
2025-09-18 08:15:48 -0300

    Funcion para convertir a porcentaje

---
+++ Biblioteca/LibreriaFunciones.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class LibreriaFunciones
+    {
+        /// <summary>
+        /// Convierte un numero a un porcentaje teniendo en cuenta
+        /// el numero maximo total del cual pertenece
+        /// </summary>
+        /// <param name="argNumberToConvert"></param>
+        /// <param name="argMaxValue"></param>
+        /// <returns></returns>
+        public float ConvertToPercentage(int argNumberToConvert, int argMaxValue)
+        {
+            return argNumberToConvert * 100 / argMaxValue;
+
+        }
+    }
+}
